078178f7baf793d62cf3d1b816fe0f5fb5ad3f66
MariyaMoh <alshaibi199732@gmail.com>
2022-09-11 18:02:34 +0200

    ReadyReaaady

--- pages/contactform.tsx
+++ pages/contactform.tsx
@@ -1,44 +1,44 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+// import React, { useState } from 'react';
+// import axios from 'axios';
 
-function App() {
-  const [email, setEmail] = React.useState('');
-  const [message, setMessage] = React.useState('');
+// function App() {
+//   const [email, setEmail] = React.useState('');
+//   const [message, setMessage] = React.useState('');
 
-  const submitHandler = async (e: any) => {
-    console.log(email, message);
-    const data = await axios.post(`/api/email`, {
-      email,
-      message,
-    });
+//   const submitHandler = async (e: any) => {
+//     console.log(email, message);
+//     const data = await axios.post(`/api/email`, {
+//       email,
+//       message,
+//     });
 
-    return (
-      <div className="App">
-        <div>
-          <form onSubmit={submitHandler}>
-            <label htmlFor="message">your message</label>
-            <input
-              onChange={(e) => setMessage(e.target.value)}
-              type="text"
-              id="message"
-              name="message"
-              placeholder="I want to talk to you"
-            />
-            <label htmlFor="email">Your email</label>
-            <input
-              onChange={(e) => setEmail(e.target.value)}
-              type="email"
-              id="email"
-              name="email"
-              placeholder="example@gmail.com"
-              required
-            />
+//     return (
+//       <div className="App">
+//         <div>
+//           <form onSubmit={submitHandler}>
+//             <label htmlFor="message">your message</label>
+//             <input
+//               onChange={(e) => setMessage(e.target.value)}
+//               type="text"
+//               id="message"
+//               name="message"
+//               placeholder="I want to talk to you"
+//             />
+//             <label htmlFor="email">Your email</label>
+//             <input
+//               onChange={(e) => setEmail(e.target.value)}
+//               type="email"
+//               id="email"
+//               name="email"
+//               placeholder="example@gmail.com"
+//               required
+//             />
 
-            <button type="submit"></button>
-          </form>
-        </div>
-      </div>
-    );
-  };
-}
-export default App;
+//             <button type="submit"></button>
+//           </form>
+//         </div>
+//       </div>
+//     );
+//   };
+// }
+// export default App;
